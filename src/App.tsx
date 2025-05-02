21b50e0b8dfcf1d8cc3b80fe27f477bd173c6718
Dahyun (Dany) <88699158+Ko-dany@users.noreply.github.com>
2025-05-02 19:04:24 -0400

    Implement initial Google Login with Firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import { useState } from "react";
-import { db, addDoc, collection } from "./firebase";
+import { db, auth, addDoc, collection } from "./firebase";
+import { User, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 function App() {
+  const [user, setUser] = useState<User | null>(null);
+
   const [name, setName] = useState("");
   const [loading, setLoading] = useState(false);
   const [success, setSuccess] = useState(false);
+
+  const handleGoogleLogin = async () => {
+    const provider = new GoogleAuthProvider();
+    await signInWithPopup(auth, provider)
+      .then((data) => {
+        console.log(data);
+        const user = data.user;
+        setUser(user);
+      })
+      .catch((error) => console.log(error));
+  };
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -28,21 +42,31 @@ function App() {
   return (
     <>
       <div>
-        <h1>Name Form</h1>
-        <form onSubmit={handleSubmit}>
-          <label>
-            Name:{" "}
-            <input
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </label>
-          <button type="submit" disabled={loading}>
-            {loading ? "Saving..." : "Save"}
-          </button>
-          {success && <p style={{ color: "green" }}>Saved successfully!</p>}
-        </form>
+        <h1>Job Application Tracker</h1>
+        {user ? (
+          <form onSubmit={handleSubmit}>
+            <h2>Welcome, {user.displayName}</h2>
+            <p>Email: {user.email}</p>
+            <img src={user.photoURL!} alt="Profile" />
+            <br />
+            <label>
+              Name:{" "}
+              <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </label>
+            <button type="submit" disabled={loading}>
+              {loading ? "Saving..." : "Save"}
+            </button>
+            {success && <p style={{ color: "green" }}>Saved successfully!</p>}
+          </form>
+        ) : (
+          <div>
+            <button onClick={handleGoogleLogin}>Login in Google</button>
+          </div>
+        )}
       </div>
     </>
   );
