d0f31293b6d739e5e63bd87d8134fda3b20b2227
Dahyun (Dany) <88699158+Ko-dany@users.noreply.github.com>
2025-05-02 22:11:03 -0400

    Maintain user login state across sessions, so it's still logged in after closing the tab or browser

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { GoogleLoginButton } from "./components/GoogleLoginButton";
 import { ApplicationForm } from "./components/ApplicationForm";
-import { User } from "firebase/auth";
+import {
+  browserLocalPersistence,
+  onAuthStateChanged,
+  setPersistence,
+  User,
+} from "firebase/auth";
+import { auth } from "./firebase";
 
 function App() {
   const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setPersistence(auth, browserLocalPersistence).then(() => {
+      const unsubscribe = onAuthStateChanged(auth, (user) => {
+        if (user) setUser(user);
+        else setUser(null);
+        setLoading(false);
+      });
+      return () => unsubscribe();
+    });
+  }, []);
 
   return (
     <>
       <div>
         <h1>Job Application Tracker</h1>
-        {user ? (
+        {loading ? (
+          <p>Loading...</p>
+        ) : user ? (
           <ApplicationForm user={user} />
         ) : (
           <GoogleLoginButton onLogin={setUser} />
