c17d2c24af0fd22b9d8e748ca1472aad843a98a4
Dahyun (Dany) <88699158+Ko-dany@users.noreply.github.com>
2025-05-12 12:18:48 -0400

    Set up HTTPS for localhost

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import fs from "fs";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,5 +11,13 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
+  },
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, "cert/key.pem")),
+      cert: fs.readFileSync(path.resolve(__dirname, "cert/cert.pem")),
+    },
+    host: "localhost",
+    port: 5173,
   },
 });
